e0d2d2daeef6913c51598fa1c448f8bdc4542310
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-12-09 22:43:37 +0100

    [add] fetch

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 require("dotenv").config();
-const { Telegraf, Markup } = require('telegraf');
+import { parse } from "node-html-parser";
+const { Telegraf } = require("telegraf");
+const fs = require("fs");
 
 const bot = new Telegraf(process.env.TOKEN);
 
-const pollOptions = ['oui', 'non'];
+const pollOptions = ["oui", "non"];
+const messageText = "?";
+const chatId = process.env.CHAT_ID;
 
-async function telegram() {
-    const messageText = "?"
-    const chatId = process.env.CHAT_ID;
+async function fetchDate(url) {
+  const response = await fetch(url);
+  const data = await response.text();
 
-    bot.telegram.sendPoll(parseInt(chatId), messageText , pollOptions)
-
-    bot.launch();
+  console.log(data);
 }
 
-telegram();
+fetchDate("https://www.lac-bleu.ch/programmes/#6");
 
+async function telegram() {
+  bot.telegram.sendPoll(parseInt(chatId), messageText, pollOptions);
+  bot.launch();
+}
+
+//telegram();
