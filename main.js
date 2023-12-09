c8afb3dc99823f020f0fd8034547eca4336db3ab
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-12-10 00:43:56 +0100

    [add] polling system

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
 require("dotenv").config();
-import { parse } from "node-html-parser";
+const parser = require('node-html-parser');
 const { Telegraf } = require("telegraf");
 const fs = require("fs");
 
 const bot = new Telegraf(process.env.TOKEN);
 
-const pollOptions = ["oui", "non"];
-const messageText = "?";
+const pollOptions = ["moi", "pas moi"];
 const chatId = process.env.CHAT_ID;
 
 async function fetchDate(url) {
   const response = await fetch(url);
   const data = await response.text();
+  const root = parser.parse(data);
 
-  console.log(data);
-}
+  let DateLys = parser.parse(root.querySelector("#7").nextElementSibling.getElementsByTagName("table"))
 
-fetchDate("https://www.lac-bleu.ch/programmes/#6");
+  DateLys.getElementsByTagName("tr").forEach(trElement => {
+    if (trElement.classList.toString() != "old") {
+        let info = parser.parse(trElement).getElementsByTagName("td").toString().replaceAll("<td>", "").replaceAll("</td>", "").split(",")
 
-async function telegram() {
-  bot.telegram.sendPoll(parseInt(chatId), messageText, pollOptions);
+        if (info[1] != undefined && parser.parse(info[1]).querySelector("span").classList.toString() != "conge") {
+            console.log(parser.parse(info[1]).querySelector("span").classList.toString());
+            console.log(info[0]);
+
+            let infoSecondary = ""
+
+            console.log(info[1].split("<br>").length);
+
+            if (info[1].split("<br>").length == 2) {
+                infoSecondary = `(${parser.parse(info[1].split("<br>")[1]).text.toString()})`;
+            }
+
+            let messageText = `Qui sera présent à : ${parser.parse(info[1].split("<br>")[0]).text.toString()} ${infoSecondary} le ${info[0].replaceAll("<br>", ", ")}`;
+            bot.telegram.sendPoll(parseInt(chatId), messageText, pollOptions); 
+        }
+    }
+  });
   bot.launch();
 }
 
-//telegram();
+fetchDate("https://www.lac-bleu.ch/programmes/#6");
